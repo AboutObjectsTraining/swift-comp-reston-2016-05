8f7f17ebd4e1f9ae320e7d0849ce092cd2254740
Jonathan Lehr <jonathan@aboutobjects.com>
2016-05-17 10:55:49 -0400

    Add highlighted property and other touch event phase methods.

--- Coolness/Coolness/CoolViewCell.swift
+++ Coolness/Coolness/CoolViewCell.swift
@@ -17,10 +17,39 @@ extension UIView
 
 class CoolViewCell: UIView
 {
+    var highlighted: Bool = false {
+        willSet { alpha = newValue ? 0.5 : 1.0 }
+    }
+}
+
+
+// MARK: - UIResponder
+
+extension CoolViewCell
+{
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        guard let touch = touches.first, view = touch.view, owningView = view.superview else { return }
+        
+        owningView.bringSubviewToFront(view)
+        highlighted = true
+    }
+    
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?)
     {
         guard let touch = touches.first, view = touch.view, owningView = view.superview else { return }
         
         view.panBy(dx: touch.deltaX, dy: touch.deltaY, constrainedTo: owningView.bounds)
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        reset()
+    }
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        reset()
+    }
+    
+    func reset() {
+        highlighted = false
     }
 }
