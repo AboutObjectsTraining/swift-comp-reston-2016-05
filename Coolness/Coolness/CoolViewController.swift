53555b229ab8ef70fd492ec7965caedf6644be87
Jonathan Lehr <jonathan@aboutobjects.com>
2016-05-18 08:07:25 -0400

    Connect textField and button

--- Coolness/Coolness/CoolViewController.swift
+++ Coolness/Coolness/CoolViewController.swift
@@ -4,7 +4,16 @@ let accessoryHeight: CGFloat = 66
 
 class CoolViewController: UIViewController
 {
-    // TODO: Add contentView property
+    var contentView: CoolView!
+    var textField: UITextField!
+    
+    func addCoolView()
+    {
+        let newCell = CoolViewCell()
+        newCell.text = textField.text
+        newCell.sizeToFit()
+        contentView.addSubview(newCell)
+    }
     
     override func loadView()
     {
@@ -15,7 +24,9 @@ class CoolViewController: UIViewController
         let contentRect = CGRect(x: frameRect.minX, y: accessoryHeight, width: frameRect.width, height: frameRect.height - accessoryHeight)
         
         let accessoryView = UIView(frame: accessoryRect)
-        let contentView = CoolView(frame: contentRect)
+        contentView = CoolView(frame: contentRect)
+        
+        configureControls(accessoryView)
         
         view.addSubview(accessoryView)
         view.addSubview(contentView)
@@ -23,5 +34,23 @@ class CoolViewController: UIViewController
         view.backgroundColor = UIColor.brownColor()
         accessoryView.backgroundColor = UIColor(white: 1, alpha: 0.8)
         contentView.backgroundColor = UIColor(white: 1, alpha: 0.65)
+    }
+    
+    func configureControls(accessoryView: UIView)
+    {
+        textField = UITextField(frame: CGRect(x: 8, y: 28, width: 240, height: 30))
+        textField.borderStyle = .RoundedRect
+        textField.placeholder = "Enter some text"
+        
+        let button = UIButton(type: .System)
+        button.setTitle("Add", forState: .Normal)
+        button.sizeToFit()
+        
+        button.addTarget(self, action: "addCoolView", forControlEvents: .TouchUpInside)
+        
+        button.frame.offsetInPlace(dx: 265, dy: 28)
+        
+        accessoryView.addSubview(textField)
+        accessoryView.addSubview(button)
     }
 }
